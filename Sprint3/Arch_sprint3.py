0d1fd0192a1b7805a6622ce638babc2fe1b94ef8
Alberto <48825796+FlorAber@users.noreply.github.com>
2025-11-20 14:41:35 +0100

    Updated Sprint3 documentation

--- Sprint3/Arch_sprint3.py
+++ Sprint3/Arch_sprint3.py
@@ -26,9 +26,9 @@ with Diagram('sprint3Arch', show=False, outformat='png', graph_attr=graphattr) a
      sys = Custom('','./qakicons/system.png')
 ### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
      with Cluster('ctx_webgui', graph_attr=nodeattr):
-          webguiobserver=Custom('webguiobserver','./qakicons/symActorWithobjSmall.png')
+          holdobserver=Custom('holdobserver','./qakicons/symActorWithobjSmall.png')
      with Cluster('ctx_cargo', graph_attr=nodeattr):
           holdmanager=Custom('holdmanager(ext)','./qakicons/externalQActor.png')
-     sys >> Edge( label='holdupdated', **evattr, decorate='true', fontcolor='darkgreen') >> webguiobserver
-     webguiobserver >> Edge(color='magenta', style='solid', decorate='true', label='<getholdstate &nbsp; >',  fontcolor='magenta') >> holdmanager
+     sys >> Edge( label='holdupdated', **evattr, decorate='true', fontcolor='darkgreen') >> holdobserver
+     holdobserver >> Edge(color='magenta', style='solid', decorate='true', label='<getholdstate &nbsp; >',  fontcolor='magenta') >> holdmanager
 diag
--- Sprint3/sprint3.pl
+++ Sprint3/sprint3.pl
@@ -7,5 +7,5 @@ event( holdupdated, holdupdated(JSONSTATE) ).
 context(ctx_webgui, "localhost",  "TCP", "8025").
 context(ctx_cargo, "127.0.0.1",  "TCP", "8014").
  qactor( holdmanager, ctx_cargo, "external").
-  qactor( webguiobserver, ctx_webgui, "it.unibo.webguiobserver.Webguiobserver").
- static(webguiobserver).
+  qactor( holdobserver, ctx_webgui, "it.unibo.holdobserver.Holdobserver").
+ static(holdobserver).
